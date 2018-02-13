ed70cd0cf17315c4bf4e2bc28d5a727c7eaaf455
Adam Lukens <adam@testdouble.com>
2018-02-13 04:26:18 -0500

    Cache Spotify queries and log timings

--- nika_tunes.rb
+++ nika_tunes.rb
@@ -8,9 +8,22 @@ require 'dotenv'
 Dotenv.load
 $logger = Logger.new(STDOUT)
 
+def log(msg, &block)
+  start_time = Time.now
+  $logger.info(msg)
+  result = block.call
+  end_time = Time.now
+  $logger.info("done. Took #{end_time-start_time} seconds")
+
+  result
+end
+
 class Jukebox
-  def initialize(speaker_name)
+  attr_reader :spotify
+
+  def initialize(speaker_name, spotify)
     @speaker_name = speaker_name
+    @spotify = spotify
   end
 
   # Play a given spotify URI on the Sonos speaker
@@ -24,32 +37,29 @@ class Jukebox
   end
 
   # Play a given RSpotify::Playlist on the Sonos speaker
-  def play_spotify_playlist(playlist)
-    uris = playlist.tracks.map(&:uri)
+  def play_spotify_playlist(user, id)
+    uris = log("Loading playlist \"#{user} - #{id}\" from spotify") do
+      spotify.playlist_uris(user, id)
+    end
     play_spotify_uris(uris)
   end
 
   # Search for a track on Spotify by title and play it on the Sonos speaker
   def play_spotify_track(title)
-    $logger.info "Retrieving track \"#{title}\" from Spotify..."
-    track = RSpotify::Track.search(title).first
-    $logger.info 'done.'
-    artist = track.artists.map(&:name).join(" & ")
-
-    $logger.info "Found #{artist} - #{track.name} from the album \"#{track.album.name}\""
-    play_spotify_uris([track.uri])
+    uri = log("Loading track \"#{title}\" from spotify") do
+      spotify.track_uri(title)
+    end
+    play_spotify_uris([uri])
   end
 
   private
   def speaker
     return @speaker if @speaker
 
-    $logger.info "Finding speaker \"#{@speaker_name}\"..."
-    system = Sonos::System.new
-    @speaker = system.speakers.find{|speaker| speaker.name == @speaker_name}
-    $logger.info 'done'
-
-    @speaker
+    @speaker = log("Finding speaker \"#{@speaker_name}\"...") do
+      system = Sonos::System.new
+      system.speakers.find{|speaker| speaker.name == @speaker_name}
+    end
   end
 
   def sonos_uri_from_spotify_uri(spotify_uri)
@@ -59,25 +69,54 @@ class Jukebox
   end
 end
 
-# TODO: Cache lookups to Spotify for performance reasons?
-# Provides a slightly simpler interface to Spotify than RSpotify
+# Provides a slightly simpler interface to Spotify than RSpotify. Now with
+# caching!
 class Spotify
   def initialize
-    @playlist_cache = {}
     @track_cache = {}
+    @playlist_cache = {}
   end
+
+  # You should initialize with this factory method to ensure that we can make
+  # authenticated queries to Spotify
+  def self.authenticate!
+    log 'Authenticating with Spotify...' do
+      client_id = ENV.fetch("SPOTIFY_CLIENT_ID")
+      client_secret = ENV.fetch("SPOTIFY_CLIENT_SECRET")
+      RSpotify.authenticate(client_id, client_secret)
+    end
+
+    new
+  end
+
+  def track_uri(title)
+    return @track_cache[title] if @track_cache[title]
+
+    track = RSpotify::Track.search(title).first
+    artist = track.artists.map(&:name).join(" & ")
+    $logger.info "Found #{artist} - #{track.name} from the album \"#{track.album.name}\""
+
+    @track_cache[title] = track.uri
+  end
+
+  def playlist_uris(user, id)
+    key = [user, id].join(':')
+    return @playlist_cache[key] if @playlist_cache[key]
+
+    playlist = RSpotify::Playlist.find("zdwiggins", "4m2vrzVCUjvrHzaW00Skli")
+    @playlist_cache[key] = playlist.tracks.map(&:uri)
+  end
+
+  # Ensure we only initialize using .authenticate
+  private_class_method :new
 end
 
-$logger.info 'Authenticating with Spotify...'
-client_id = ENV.fetch("SPOTIFY_CLIENT_ID")
-client_secret = ENV.fetch("SPOTIFY_CLIENT_SECRET")
-RSpotify.authenticate(client_id, client_secret)
-$logger.info 'done'
-$logger.info 'Finding playlist on Spotify...'
-playlist = RSpotify::Playlist.find("zdwiggins", "4m2vrzVCUjvrHzaW00Skli")
-$logger.info 'done'
+speaker_name = ENV.fetch("SPEAKER_NAME")
+spotify = Spotify.authenticate!
+jukebox = Jukebox.new(speaker_name, spotify)
 
-jukebox = Jukebox.new(ENV.fetch("SPEAKER_NAME"))
+# Bedtime music
+jukebox.play_spotify_playlist("zdwiggins", "4m2vrzVCUjvrHzaW00Skli")
 
 loop do
   print 'Title: '
